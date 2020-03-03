101be25344c98bae1019180602cbadf3e80bb000
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-03-03 12:00:56 +0900

    installed pug and edit login page for pug

--- pages/login.vue
+++ pages/login.vue
@@ -1,90 +1,31 @@
-<template>
-  <v-row tag="section" class="login">
-    <v-col v-text="title.toUpperCase()" tag="h1" cols="12" class="text-center" />
-    <v-col v-if="loginPage" cols="12" md="4" offset-md="4">
-      <v-form>
-        <v-text-field
-          v-model="email"
-          label="E-mail"
-          required
-        />
-        <v-text-field
-          v-model="password"
-          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show ? 'text' : 'password'"
-          @click:append="show = !show"
-          label="Password"
-          required
-        />
-        <v-row>
-          <v-col>
-            <v-btn
-              @click="login"
-              color="success"
-              class="mr-4"
-            >
-              {{ title }}
-            </v-btn>
-          </v-col>
-          <v-spacer />
-          <v-col>
-            <v-btn @click="switchPage" text dark>
-              {{ switchButton }}
-              <v-icon dark right>
-                mdi-arrow-right
-              </v-icon>
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-form>
-    </v-col>
-    <v-col v-else cols="12" md="4" offset-md="4">
-      <v-form>
-        <v-text-field
-          v-model="email"
-          label="E-mail"
-          required
-        />
-        <v-text-field
-          v-model="password"
-          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show ? 'text' : 'password'"
-          @click:append="show = !show"
-          label="Password"
-          required
-        />
-        <v-text-field
-          v-model="passwordConfirm"
-          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show ? 'text' : 'password'"
-          @click:append="show = !show"
-          label="Password(confirm)"
-          required
-        />
-        <v-row>
-          <v-col>
-            <v-btn
-              @click="login"
-              color="success"
-              class="mr-4"
-            >
-              {{ title }}
-            </v-btn>
-          </v-col>
-          <v-spacer />
-          <v-col>
-            <v-btn @click="switchPage" text dark>
-              {{ switchButton }}
-              <v-icon dark right>
-                mdi-arrow-right
-              </v-icon>
-            </v-btn>
-          </v-col>
-        </v-row>
-        {{ signUpError }}
-      </v-form>
-    </v-col>
-  </v-row>
+<template lang="pug">
+  v-row.login(tag="section")
+    v-col.text-center(v-text="title.toUpperCase()" tag="h1" cols="12")
+    v-col(v-if="loginPage" cols="12" md="4" offset-md="4")
+      v-form
+        v-text-field(v-model="email" label="E-mail" required)
+        v-text-field(v-model="password" :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'" :type="show ? 'text' : 'password'" @click:append="show = !show" label="Password" required)
+        v-row
+          v-col
+            v-btn.mr-4(@click="login" color="success") {{ title }}
+          v-spacer
+          v-col
+            v-btn(@click="switchPage" text dark) {{ switchButton }}
+              v-icon(dark right) mdi-arrow-right
+    v-col(v-else cols="12" md="4" offset-md="4")
+      v-form
+        v-text-field(v-model="email" label="E-mail" required)
+        v-text-field(v-model="password" :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'" :type="show ? 'text' : 'password'" @click:append="show = !show" label="Password" required)
+        v-text-field(v-model="passwordConfirm" :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'" :type="show ? 'text' : 'password'" @click:append="show = !show" label="Password(confirm)" required)
+        v-row
+          v-col
+            v-btn.mr-4(@click="login" color="success") {{ title }}
+          v-spacer
+          v-col
+            v-btn(@click="switchPage" text dark) {{ switchButton }}
+              v-icon(dark right) mdi-arrow-right
+        v-row(v-if="[...signUpError].length")
+          v-col {{ signUpError }}
 </template>
 
 <script>
